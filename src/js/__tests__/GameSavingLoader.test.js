cc85429c022f6a18fd594b13c6a6864055459201
kkk <kutimskyi@gmail.com>
2023-07-02 21:12:41 +0300

    second

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -3,6 +3,12 @@ import GameSavingLoader from '../GameSavingLoader';
 jest.setTimeout(15000);
 test('test  GameSavingLoader ', async (done) => {
   const saving = await GameSavingLoader.load();
-  expect(saving).toEqual({"created": 1546300800, "id": 9, "userInfo": {"id": 1, "level": 10, "name": "Hitman", "points": 2000}});
+  expect(saving).toEqual({
+    created: 1546300800,
+    id: 9,
+    userInfo: {
+      id: 1, level: 10, name: 'Hitman', points: 2000,
+    },
+  });
   done();
 });
